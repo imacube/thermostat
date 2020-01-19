fdca3f5d58e4c4e8238ab994644a736daa3f788d
Ryan <imacube@gmail.com>
2020-01-19 11:06:53 -0800

    Further work on getting things working

--- src/test.cpp
+++ src/test.cpp
@@ -3,12 +3,15 @@
 // #include "Thermostat.h"
 
 #include <Arduino.h>
+#include <MockSerial.h>
 // #include "Arduino.h"
 // #include "add.h"
 
 int main() {
   std::cout << "hello world!\n";
   std::cout << millis();
+  Serial.begin(9600);
+  Serial.write("Foobar");
   // std::cout << add(1, 2) << "\n";
   return 0;
 }
